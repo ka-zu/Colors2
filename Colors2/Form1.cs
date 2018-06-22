0ce211478b47da4382b20ce438e96fe0a4eae540
ka-zu <sprite13return@gmail.com>
2018-06-22 14:07:10 +0900

    set button

--- Colors2/Form1.cs
+++ Colors2/Form1.cs
@@ -16,5 +16,20 @@ namespace Colors2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
