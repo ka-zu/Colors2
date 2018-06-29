add94559e30ec3fe9815f002866dd401f4aa4b8c
ka-zu <sprite13return@gmail.com>
2018-06-29 17:51:22 +0900

    プロジェクション画面を最大化で表示するように。 qキーで終了できるようにした。

--- Colors2/Form2.Designer.cs
+++ Colors2/Form2.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form2_KeyPress);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Colors2/Form2.cs
+++ Colors2/Form2.cs
@@ -20,11 +20,53 @@ namespace Colors2
 
         //接続されているスクリーンを取得
         private Screen[] screen = System.Windows.Forms.Screen.AllScreens;
-        
+
+        //ウィンドウの状態
+        private FormWindowState formState = FormWindowState.Normal;
+
+
+        //通常時のウィンドウサイズを保存
+        private Size prevFormSize;
+
 
         public Form2()
         {
             InitializeComponent();
+
+            this.KeyPreview = true;
+
+            //ウィンドウの状態を保存
+            formState = this.WindowState;
+
+            //最大化　＝＞　フルスクリーンではタスクバーが消えないので
+            //いったん通常表示
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+
+            //フォームサイズを保存
+            prevFormSize = this.ClientSize;
+
+            //境界線スタイルをなくす
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            //ウィンドウサイズを最大化
+            this.WindowState = FormWindowState.Maximized;
+
+            isFullScreenMode = true;
+        }
+
+        //キー入力を判定する
+        private void Form2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case 'q'://投影フォームを閉じる
+                    this.Close();
+                    break;
+            }
+
         }
     }
 }
