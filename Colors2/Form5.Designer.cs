f242d54abd55222d04c7c395f23d75db355b8e0b
Naoki Kanyama <yamakan1213@icloud.com>
2018-07-12 15:18:05 +0900

    objフォルダはいらない それとForm5の中身を不必要な奴は空っぽにした

--- Colors2/Form5.Designer.cs
+++ Colors2/Form5.Designer.cs
@@ -29,25 +29,19 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // timer
-            // 
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
-            // 
-            // Form2
+            // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.DoubleBuffered = true;
-            this.Name = "Form2";
-            this.Text = "投影";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form5_FormClosing);
+            this.Name = "Form5";
+            this.Text = "画像受信";
             this.Load += new System.EventHandler(this.Form5_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form5_Paint);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form5_KeyPress);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form5_FormClosing);
             this.ResumeLayout(false);
 
         }
--- Colors2/Form5.cs
+++ Colors2/Form5.cs
@@ -25,17 +25,8 @@ namespace Colors2
         private int height = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
         private int width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
 
-        //接続されているスクリーンを取得
-        private Screen[] screen = System.Windows.Forms.Screen.AllScreens;
-
         //ウィンドウの状態
         private FormWindowState formState = FormWindowState.Normal;
-
-        //座標
-        Point p1 = new Point();
-
-        //設定画面で選択された画像の受け取り用
-        private string[] filePath;
 
         public Form5()
         {
@@ -56,43 +47,6 @@ namespace Colors2
             formState = this.WindowState;
         }
 
-        private void Form5_Load(object sender, EventArgs e)
-        {
-            //タイマーの初期化
-            initTimer();
-        }
-
-        //キー入力を判定する
-        private void Form5_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            switch (e.KeyChar)
-            {
-                case 'q'://ウィンドウの境界を消す
-                    cahngeBorderStyle();
-                    break;
-
-                case (char)Keys.Escape://フォームを閉じる
-                    this.Close();
-                    break;
-            }
-        }
-
-        //描画管理
-        private void drawPicture(PaintEventArgs e)
-        {
-
-            //仮画像を割り当て
-            Image img1 = Image.FromFile("../../figureImages/circle_orange.png");
-
-            e.Graphics.DrawImage(img1, p1);
-        }
-
-        private void Form5_Paint(object sender, PaintEventArgs e)
-        {
-            //描画メソッドの呼び出し
-            drawPicture(e);
-        }
-
         private void Form5_FormClosing(object sender, FormClosingEventArgs e)
         {
             //閉じるボタンを押しても再表示できるように
@@ -102,76 +56,14 @@ namespace Colors2
                 this.Visible = false;
             }
         }
-
         
-
-        //ウィンドウの境界を消す
-        private void cahngeBorderStyle()
+        private void Form5_Load(object sender, EventArgs e)
         {
-            if (isFullScreenMode == false)
-            {
-                //フルスクリーンに変更
-
-                //ウィンドウの状態を保存
-                prevFormState = this.WindowState;
-                //境界線スタイルを保存
-                prevFormStyle = this.FormBorderStyle;
-
-                //最大化　＝＞　フルスクリーンではタスクバーが消えないので
-                //いったん通常表示
-                if (this.WindowState == FormWindowState.Maximized)
-                {
-                    this.WindowState = FormWindowState.Normal;
-                }
-
-                //フォームサイズを保存
-                prevFormSize = this.ClientSize;
-
-                //境界線スタイルをなくす
-                this.FormBorderStyle = FormBorderStyle.None;
-
-                //ウィンドウサイズを最大化
-                //this.WindowState = FormWindowState.Maximized;
-
-                isFullScreenMode = true;
-            }
-            else
-            {
-                //最大化に戻すときはいったん通常表示にする
-                //（フルスクリーン処理との関係のため）
-                if (this.WindowState == FormWindowState.Maximized)
-                {
-                    this.WindowState = FormWindowState.Normal;
-                }
-
-                //フォームの境界線スタイルを元に戻す
-                this.FormBorderStyle = prevFormStyle;
-
-                //フォームのウィンドウ状態をもとにもどす
-                this.WindowState = prevFormState;
-
-                isFullScreenMode = false;
-            }
         }
 
-        //時間制御初期化
-        private void initTimer()
+        private void Form5_Paint(object sender, PaintEventArgs e)
         {
-            //タイマーの動作時間
-            timer.Interval = 100;
-            //時間で動く関数の呼び出し
-            timer.Tick += new EventHandler(timer_Tick);
-
-            //タイマーを有効化
-            timer.Enabled = true;
         }
 
-        //時間制御
-        private void timer_Tick(object sender, EventArgs e)
-        {
-            p1.X += 1;
-            p1.Y += 1;
-            Refresh();
-        }
     }
 }
