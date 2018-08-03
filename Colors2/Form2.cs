afc5d63c69db05e72ec0e17e25818489ba5484fe
ka-zu <sprite13return@gmail.com>
2018-08-03 17:36:53 +0900

    図形の動き完成 ・画面端での反転　revSpeed ・中心座標を求める関数の追加　setCenterPoint ・フォームを全画面にしてもサイズが変わらないように ・投影する画像のサイズを一定にした IMG_SIZE_(X,Y) ・画像割り当て部を新しく ・動きを与える関数を作成　movement ・初期位置を与える関数　initFigurePoint

--- Colors2/Form2.cs
+++ Colors2/Form2.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;//ファイル読み取り
 
+
 namespace Colors2
 {
     public partial class Form2 : Form
@@ -31,13 +32,7 @@ namespace Colors2
 
         //ウィンドウの状態
         private FormWindowState formState = FormWindowState.Normal;
-
-        //座標
-        Point p1 = new Point();
-
-        //設定画面で選択された画像の受け取り用
-        private string[] imagePath;
-
+        
         //動的配列の宣言
         List<String> strList = new List<String>();
 
@@ -46,6 +41,13 @@ namespace Colors2
 
         //デフォルトで表示される図形
         Figure initFig = new Figure(@"../../logo/Colors_logo2.png");
+
+        //サイン波動作の角度用
+        double angle = 0;
+
+        //投影する図形の最大サイズ
+        private const int IMG_SIZE_X = 150;
+        private const int IMG_SIZE_Y = 150;
 
         public Form2()
         {
@@ -73,7 +75,12 @@ namespace Colors2
             //タイマーの初期化
             initTimer();
            
+            //設定した画像から図形オブジェクトを生成
             makeFigureObj();
+            //ウィンドウサイズに合わせて初期座標を変更           
+            initFigurePoint( figList );
+         
+                
         }
 
         //キー入力を判定する
@@ -86,6 +93,7 @@ namespace Colors2
                     break;
 
                 case (char)Keys.Escape://フォームを閉じる
+                    //this.Visible = false;
                     this.Close();
                     break;
             }
@@ -98,13 +106,15 @@ namespace Colors2
             if(figList[0] != null)
             {
                 foreach (Figure temp in figList ){
-
-                    e.Graphics.DrawImage(temp.img,temp.p);
+                    
+                    e.Graphics.DrawImage(temp.img,temp.point.X,temp.point.Y,IMG_SIZE_X,IMG_SIZE_Y);
+                    //e.Graphics.DrawArc(Pens.Red,temp.point.X,temp.point.Y,10,10,0,360);座標確認用
+                    //e.Graphics.DrawArc(Pens.Blue, temp.centerPoint.X, temp.centerPoint.Y, 10, 10, 0, 360);//中心座標確認用
                 }
             }
             else
             {
-                e.Graphics.DrawImage(initFig.img, initFig.p);
+                e.Graphics.DrawImage(initFig.img, initFig.point.X, initFig.point.Y,IMG_SIZE_X,IMG_SIZE_Y);
             }
 
         }
@@ -118,11 +128,12 @@ namespace Colors2
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
             //閉じるボタンを押しても再表示できるように
-            if (e.CloseReason == CloseReason.UserClosing)
+            /*if (e.CloseReason == CloseReason.UserClosing)
             {
                 e.Cancel = true;
                 this.Visible = false;
-            }
+            }*/
+            //this.Close();//closingにcloseを書くと無限ループしてエラー
         }
 
         //ウィンドウの境界を消す
@@ -153,10 +164,18 @@ namespace Colors2
                 //ウィンドウサイズを最大化
                 //this.WindowState = FormWindowState.Maximized;
 
+                //境界線が消えた分を補完
+                this.Height += 32;
+                this.Left += 8;
+
                 isFullScreenMode = true;
             }
             else
             {
+                //境界線が増える分を補完
+                this.Height -= 32;
+                this.Left -= 8;
+
                 //最大化に戻すときはいったん通常表示にする
                 //（フルスクリーン処理との関係のため）
                 if (this.WindowState == FormWindowState.Maximized)
@@ -178,7 +197,7 @@ namespace Colors2
         private void initTimer()
         {
             //タイマーの動作時間
-            timer.Interval = 100;
+            timer.Interval = 50;
             //時間で動く関数の呼び出し
             timer.Tick += new EventHandler(timer_Tick);
 
@@ -189,19 +208,32 @@ namespace Colors2
         //時間制御
         private void timer_Tick(object sender, EventArgs e)
         {
-           
+            //角度が一周したら
+            if(angle == 360)
+            {
+                angle = 1;
+            }
+            else
+            {
+                angle += 5;
+            }
+
+
             if (figList[0] != null)
             {
                 foreach (Figure temp in figList)
                 {
-                    temp.p.X += 1;
-                    temp.p.Y += 1;
+                    //temp.point.X += 1;
+                    //temp.point.Y += 1;
+                   
+                    movement(temp);
+                    revSpeed(temp);
                 }
             }
             else
             {
-                initFig.p.X += 1;
-                initFig.p.Y += 1;
+                revSpeed(initFig);
+                movement(initFig);
             }
             Refresh();
         }
@@ -212,22 +244,138 @@ namespace Colors2
             figList.Clear();
             try
             {
-                String str;
-                StreamReader reader = new StreamReader(@"./selectLog.txt");
-
-                strList.Clear();
-                while ((str = reader.ReadLine()) != null)
+                //設定読み込み部
+                int spe = 0; int mov = 0; int pic = 0;
+                if (!File.Exists(@"./setingLog.txt"))
                 {
-                    Figure fig = new Figure(str);
-                    figList.Add(fig);//要素を末尾に追加
-                    System.Console.WriteLine(str+"\n");
+                    String str;
+                    StreamReader readSetting = new StreamReader(@"./settingLog.txt");
+
+                    str = readSetting.ReadLine();
+                    spe = int.Parse(str);
+                    str = readSetting.ReadLine();
+                    mov = int.Parse(str);
+                    str = readSetting.ReadLine();
+                    pic = int.Parse(str);
+                    Console.WriteLine("spe = " + spe + " mov = " + mov + " pic = " + pic);
+
+                    readSetting.Close();
                 }
-                reader.Close();
+
+                //画像割り当て部
+                if (pic == 0 || pic == 1)//選択されているのが基本・オリジナル図形だったら
+                {
+                    if (!File.Exists(@"./ selectLog.txt"))
+                    {
+                        //画像を割り当て
+                        String str;
+                        StreamReader reader = new StreamReader(@"./selectLog.txt");
+
+                        strList.Clear();
+                        while ((str = reader.ReadLine()) != null)
+                        {
+                            Figure fig = new Figure(str,mov,spe);
+                            figList.Add(fig);//要素を末尾に追加
+                            Console.WriteLine(str);
+                        }
+                        reader.Close();
+                    }
+                }
+                else if(pic == 2)//最新５件だったら
+                {
+                    //後で書く
+                }
+                else
+                {
+                    //画像を割り当て
+                    String str;
+                    StreamReader reader = new StreamReader(@"./selectLog.txt");
+
+                    strList.Clear();
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        Figure fig = new Figure(str);
+                        figList.Add(fig);//要素を末尾に追加
+                        Console.WriteLine(str);
+                    }
+                    reader.Close();
+                }
+
+                
+                    
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);//コンソール出力
             }
+        }
+
+        //動きに合わせて次の座標を与える
+        private void movement(Figure fig)
+        {
+            //Console.WriteLine("Form2:" + fig.motionType);
+            switch ( fig.motionType )
+            {
+                case 0://動かない(基本バグってるときのみなるはず)
+                    break;
+                case 1://横
+                    fig.point.X += fig.speed;
+                    break;
+                case 2://縦
+                    fig.point.Y += fig.speed;
+                    break;
+                case 3://右斜め上
+                    fig.point.X += fig.speed;
+                    fig.point.Y -= fig.speed;
+                    break;
+                case 4://右斜め下
+                    fig.point.X += fig.speed;
+                    fig.point.Y += fig.speed;
+                    break;
+                case 5://サイン波
+                    fig.point.X += fig.speed;
+                    fig.point.Y += (int)(fig.speed* 2 * Math.Sin(angle * (Math.PI / 180)));
+                    break;
+                case 6://円
+                    fig.point.X += (int)(fig.speed * 2 * Math.Cos(angle * (Math.PI / 180)));
+                    fig.point.Y += (int)(fig.speed * 2 * Math.Sin(angle * (Math.PI / 180)));
+                    break;
+            }
+            setCenterPoint(fig);
+        }
+
+        //位置座標の初期化（Form2の形に合わせるためここで定義）
+        private void initFigurePoint(List<Figure> figre)
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            foreach (Figure fig in figre)
+            {
+                //左上座標
+                fig.point.X = r.Next(this.Width-IMG_SIZE_X);
+                fig.point.Y = r.Next(this.Height-IMG_SIZE_Y);
+                //中心座標
+                fig.centerPoint.X = r.Next(this.Width - IMG_SIZE_X) + (IMG_SIZE_X / 2);
+                fig.centerPoint.Y = r.Next(this.Height - IMG_SIZE_Y) + (IMG_SIZE_Y / 2);
+            }
+        }
+
+        //速度を反転
+        private void revSpeed(Figure fig)
+        {
+            
+            if(fig.centerPoint.X < 0 || fig.centerPoint.X > this.Width || fig.centerPoint.Y < 0 || fig.centerPoint.Y > this.Height)
+            {
+                Console.WriteLine("reverse speed");
+                fig.speed = (-1)*fig.speed;
+            }
+        }
+
+        //図形の座標から中心座標をセット(座標を変化させたら使う)
+        private void setCenterPoint(Figure fig)
+        {
+            fig.centerPoint.X = fig.point.X + IMG_SIZE_X / 2;
+            fig.centerPoint.Y = fig.point.Y + IMG_SIZE_Y / 2;
         }
     }
 }
