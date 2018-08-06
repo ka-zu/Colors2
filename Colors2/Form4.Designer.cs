9e8e63020b12ed2893f771b18380724847326482
ka-zu <sprite13return@gmail.com>
2018-08-06 23:42:13 +0900

    機能追加に伴って説明画面を追加

--- Colors2/Form4.Designer.cs
+++ Colors2/Form4.Designer.cs
@@ -36,11 +36,13 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -114,11 +116,20 @@
             this.pictureBox5.TabIndex = 7;
             this.pictureBox5.TabStop = false;
             // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(390, 521);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(414, 575);
+            this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
@@ -136,6 +147,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,5 +163,6 @@
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
--- Colors2/Form4.cs
+++ Colors2/Form4.cs
@@ -14,7 +14,7 @@ namespace Colors2
     {
         //ページ番号
         int pageNum = 1;
-        const int MAX_PAGE = 5;
+        const int MAX_PAGE = 6;
 
         public Form4()
         {
@@ -28,11 +28,13 @@ namespace Colors2
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.ImageLocation = @"./logo/page1.png";
             pictureBox2.ImageLocation = @"./logo/page2.png";
             pictureBox3.ImageLocation = @"./logo/page3.png";
             pictureBox4.ImageLocation = @"./logo/page4.png";
             pictureBox5.ImageLocation = @"./logo/page5.png";
+            pictureBox6.ImageLocation = @"./logo/page6.png";
 
             paging();
         }
@@ -84,6 +86,8 @@ namespace Colors2
             pictureBox3.Visible = false;
             pictureBox4.Visible = false;
             pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+
             switch (pageNum)
             {
                 case 1:
@@ -100,6 +104,11 @@ namespace Colors2
                     break;
                 case 5:
                     pictureBox5.Visible = true;
+                    break;
+                case 6:
+                    pictureBox6.Visible = true;
+                    break;
+                default:
                     break;
             }
 
